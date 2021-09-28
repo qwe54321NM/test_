bb7792f1ae013ec855e52b9988dc2554c8948b09
Madiyar <91475360+qwe54321NM@users.noreply.github.com>
2021-09-28 23:26:00 +0700

    Add files via upload
    Криптон тест №1

---
+++ test_.py
@@ -0,0 +1,11 @@
+str1 = "3333311111117115"
+str2 = "44422222221111"
+output1 = str(int(str2) + int(str1))
+with open('output1.txt', 'w') as f1:
+    f1.write(output1)
+
+strr1 = "7777555511111111"
+strr2 = "3332222222112"
+output2 = str(int(strr2) + int(strr1))
+with open('output2.txt', 'w') as f2:
+    f2.write(output2)
